d92188c8ca583159ecffcb0b660e094e99eae936
Ananthu Mahalaxmi <mahalaxmiananthu@gmail.com>
2026-02-21 22:54:53 +0530

    Add files via upload

---
+++ m.py
@@ -0,0 +1,88 @@
+# # variable
+# name="maha"
+# age=24
+# age=26
+# print(age)
+# print()
+
+
+#   num1,num2,num3=23,34,67
+#  print(num1)
+
+#  m="hello mahalxmi what's app "
+#  num1= 34
+#  print(m,num1)
+
+# # numeric data type 
+# x=3
+# y=4
+# c=(x+y)
+
+# print(type(c))
+
+# # type function
+# b=4 
+# n=4.5
+# print(b+n)
+# print(type(b))
+
+# # casting
+# num = float (b)
+# print (num)
+
+# # string
+# string="hello"
+# print(string)
+
+# print(len(string))
+
+# index
+a="start with ends with"
+print(a)
+print(a[1])
+print(a[-1])
+
+# slicing
+maha="Nature refers to the physical world and everything in it that is not made or caused by humans. "
+# # print(len(maha))
+# # print(maha[: : -1])
+# # print(maha[0:271])
+# print(maha[4: ])
+# print(maha[29:35])
+# print(maha[-12:271:4])
+# print(maha[: :-1])
+# positive slicing
+# print(maha[0:100])
+# print(maha[1:35:2])
+# print(maha[40:80])
+# # negative slicing
+# print(maha[-100:-1])
+# print(maha[-60:-1])
+# print(len(maha))
+# print(maha[-66:-60])
+# print(maha[-100:-20:2])
+# print(maha*2)
+m="Nature refers to the physical world and everything in it that is not made or caused by humans. It encompasses all living organisms, including plants, animals,\n and microorganisms, as well as non-living elements such as landscapes, weather patterns, and natural resources."
+print(m)
+print('all' in m)
+# index
+print(m.index("p"))
+# lowercase
+m="Nature refers to the physical world and everything in it that is not made or caused by humans. It encompasses all living organisms, including plants, animals, and microorganisms, as well as non-living elements such as landscapes, weather patterns, and natural resources."
+# print(m.lower())
+# print(m.upper())
+print(m.swapcase())
+
+# operators
+m=[20,30,40,50,60,"maha",True,90.45]
+print(m)
+m[2]=45
+print(m)
+print(m[5:40])
+
+
+
+
+
+
+
